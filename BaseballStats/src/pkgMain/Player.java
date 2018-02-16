165ff3bffdfa7e2bf1405d531f9bf0e0176b9434
Jonni Tran <Jonni Tran@128.4.138.34>
2018-02-15 23:05:55 -0500

    Finished

---
+++ BaseballStats/src/pkgMain/Player.java
@@ -0,0 +1,101 @@
+package pkgMain;
+
+public class Player {
+	int ab;
+	int h;
+	int _2b;
+	int _3b;
+	int hr;
+	int r;
+	int bb;
+	String name;
+	
+public Player(String playername) {
+	this.name = playername;
+}		
+
+
+	public int getAb() {
+		return ab;
+	}
+	public void setAb(int ab) {
+		this.ab = ab;
+	}
+	public int getH() {
+		return h;
+	}
+	public void setH(int h) {
+		this.h = h;
+	}
+	public int get_2b() {
+		return _2b;
+	}
+	public void set_2b(int _2b) {
+		this._2b = _2b;
+	}
+	public int get_3b() {
+		return _3b;
+	}
+	public void set_3b(int _3b) {
+		this._3b = _3b;
+	}
+	public int getHr() {
+		return hr;
+	}
+	public void setHr(int hr) {
+		this.hr = hr;
+	}
+	public int getR() {
+		return r;
+	}
+	public void setR(int r) {
+		this.r = r;
+	}
+	public int getBb() {
+		return bb;
+	}
+	public void setBb(int bb) {
+		this.bb = bb;
+	}
+	
+	//Calculates batting average with hits/at bats
+	public double battingAverage(int ab, int hits) {
+		double ba;
+		ba = hits/ab;
+		return ba;
+}
+
+	//Calculates on base percentage with (hits+walks)/(hits+walks+at bats)
+	public double onbasepercentage(int hits, int bb, int ab) {
+		double obp;
+		obp = (hits+bb)/(hits+bb+ab);
+		return obp;
+	}
+
+	/*Calculates slugging percentage with (runs+doubles+triples+homeruns)/at bats.
+	 * I used runs because hits does not translate into going onto the bases but a run does.
+	 * I used it at a single because we do not have an attribute for a single base taken.
+	 */
+	public double sluggingpercentage(int runs, int doubles, int triples, int ab, int homerun) {
+		double sp;
+		sp = (runs+doubles+triples+homerun)/ab;
+		return sp;
+	}
+	
+	//Calculates on base slugging with slugging percentage + on base percentage
+	public double onbaseslugging(double sp, double obp) {
+		double obs;
+		obs = (sp+obp);
+		return obs;
+	}
+	
+	/*Calculates total bases with doubles*2 + triples*3 + homeruns*4 + walks+ runs
+	 * Same logic from slugging percentage as walks and runs counts as taking a single base which is added to the total
+	 */
+	public int totalbases(int doubles, int triples, int homerun, int walks, int runs) {
+		int tb;
+		tb = (doubles*2)+(triples*3)+(homerun*4)+walks+runs;
+		return tb;
+}
+
+}
---
+++ BaseballStats/src/pkgMain/Stats.java
@@ -0,0 +1,49 @@
+package pkgMain;
+import java.util.Scanner;
+
+import pkgMain.Player;
+
+public class Stats {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.print("Player name: ");
+		String playername = scan.next();
+		Player p1 = new Player(playername);
+		
+		System.out.print("At Bats: ");
+		p1.setAb(scan.nextInt());
+		
+		System.out.print("Hits: ");
+		p1.setH(scan.nextInt());
+		
+		System.out.print("Doubles:  ");
+		p1.set_2b(scan.nextInt());
+		
+		System.out.print("Triples: ");
+		p1.set_3b(scan.nextInt());
+		
+		System.out.print("Home Runs: ");
+		p1.setHr(scan.nextInt());
+		
+		System.out.print("Runs: ");
+		p1.setR(scan.nextInt());
+		
+		System.out.print("Walks: ");
+		p1.setBb(scan.nextInt());
+		
+		double battingaverage = p1.battingAverage(p1.ab,p1.h);
+		double onbasepercentage = p1.onbasepercentage(p1.h, p1.bb, p1.ab);
+		double sluggingpercentage = p1.sluggingpercentage(p1.h,p1._2b,p1._3b,p1.ab,p1.hr);
+		double onbaseslugging = p1.onbaseslugging(sluggingpercentage, onbasepercentage);
+		int totalbases = p1.totalbases(p1._2b, p1._3b, p1.hr, p1.bb, p1.r);
+		
+		System.out.println("Batting average: " + battingaverage);
+		System.out.println("On base percentage: " + onbasepercentage);
+		System.out.println("Slugging percentage: " + sluggingpercentage);
+		System.out.println("On base slugging: " + onbaseslugging);
+		System.out.println("Total bases: " + totalbases);
+
+
+	}
+}
